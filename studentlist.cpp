d4307a05fc3964399dac368bffb5f8ef92ae3f0f
U-BSD\459535 <459535@SHS-34W2NW3.beaverton.k12.or.us>
2024-09-24 09:37:05 -0700

    save

--- studentlist.cpp
+++ studentlist.cpp
@@ -1,41 +1,61 @@
 #include <iostream>
 #include <vector>
 #include <cstring>
+#include <iomanip> 
 
 using namespace std;
 
 struct Student {
-    char name[100];
-    int age;
-    double grade;
+  char fname[100];
+  char lname[100];
+  int id;
+  double gpa;
 };
 
-void addStudent(vector<Student*>& students, char* name, int age, double grade) {
+void addStudent(vector<Student*>& students, char* fname, char* lname, int id, double gpa) {
     Student* student = new Student;
-    strcpy(student->name, name);  
-    student->age = age;
-    student->grade = grade;
+    strcpy(student->fname, fname);
+    strcpy(student->lname, lname);
+    student->id = id;
+    student->gpa = gpa;
     students.push_back(student);
 }
 
 void displayStudents(vector<Student*>& students) {
     for (Student* student : students) {
-        cout << "Name: " << student->name
-             << ", Age: " << student->age
-             << ", Grade: " << student->grade << endl;
+      cout << student->fname << " " << student->lname << ", " << student->id << ", " << setprecision(2) << student->gpa;
     }
 }
 
 int main() {
     vector<Student*> students;
+    char input[6];
+    while (strcmp(input, "QUIT") != 0){
+      cin.get(input, 5);
+      cin.get();
+      if (strcmp(input, "ADD") == 0){
+	char fname[10];
+	char lname[10];
+	int id;
+	double gpa;
+	cout << "First name: " << endl;
+	cin.get(fname, 9);
+	cin.get();
+	cout << "Last name: " << endl;
+	cin.get(lname, 9);
+	cin.get();
+	cout << "ID" << endl;
+	cin >> id;
+	cout << "GPA" << endl;
+	cin >> gpa;
+	addStudent(students, fname, lname, id, gpa); 
+      }
+      if(strcmp(input, "PRINT") == 0){
+	displayStudents(students);
+      }
+    }
 
-    char name1[] = "Shyam";
-    char name2[] = "sehaj";
     
-    addStudent(students, name1, 20, 85.5);
-    addStudent(students, name2, 21, 90.3);
-
-    displayStudents(students);
 
     return 0;
 }
