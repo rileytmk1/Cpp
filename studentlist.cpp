773b73b129a530f1c48ff55da6a32eae623a6049
U-BSD\459535 <459535@SHS-34W2NW3.beaverton.k12.or.us>
2024-10-04 09:25:37 -0700

    final

--- studentlist.cpp
+++ studentlist.cpp
@@ -12,6 +12,8 @@ struct Student {
   double gpa;
 };
 
+// Add structs to vector of struct pointers
+// Pass by reference so we can modify the vector
 void add(vector<Student*>& students, char* fname, char* lname, int id, double gpa) {
     Student* student = new Student;
     strcpy(student->fname, fname);
@@ -23,45 +25,52 @@ void add(vector<Student*>& students, char* fname, char* lname, int id, double gp
 
 void print(vector<Student*>& students) {
     for (Student* student : students) {
-      cout << student->fname << " " << student->lname << ", " << student->id << ", " << fixed << setprecision(2) << student->gpa << endl;
+      cout << student->fname << " " << student->lname << ", " << student->id << ", " << fixed << setprecision(2) << student->gpa << endl; //set gpa to 2 decimal points
     }
 }
 
 void del(vector<Student*>& students, int check_id){
   for (auto it = students.begin(); it != students.end(); it++){
     if((*it)->id == check_id){
-      delete *it;
-      students.erase(it);
+      delete *it; //deallocate memory for the pointer
+      students.erase(it); //remove from vector
       break;
     }
   }
 }
 
 
+
 int main() {
     vector<Student*> students;
     char input[7];
-    while (strcmp(input, "QUIT") != 0){
+    while (strcmp(input, "QUIT") != 0){ // continute until QUIT command
       cout << "Enter, ADD, PRINT, DELETE, or QUIT" << endl;
       cin.get(input, 7);
       cin.get();
+
       if (strcmp(input, "ADD") == 0){
-	char fname[10];
-	char lname[10];
+	char fname[100];
+	char lname[100];
 	int id;
 	double gpa;
+	
 	cout << "First name: ";
-	cin.get(fname, 9);
+	cin.get(fname, 99);
 	cin.get();
+	
 	cout << "Last name: ";
-	cin.get(lname, 9);
+	cin.get(lname, 99);
 	cin.get();
+	
 	cout << "ID: ";
 	cin >> id;
 	cin.ignore();
+	
 	cout << "GPA: ";
 	cin >> gpa;
 	cin.ignore();
+	
 	add(students, fname, lname, id, gpa); 
       }
       else if(strcmp(input, "PRINT") == 0){
@@ -76,8 +85,6 @@ int main() {
       }
       
     }
-
-    
 
     return 0;
 }
